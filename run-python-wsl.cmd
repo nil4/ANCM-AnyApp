db938d519910d050e9fdde2f104dc281d4f0c916
nil4 <nil4@users.noreply.github.com>
2018-08-11 20:32:47 +0200

    Update Python-WSL demo (ufortunately, still does not work under ANCM.)
    Pass ASPNETCORE_PORT=18000 env var explicitly, both as wsl.exe argument, and in Web.config.

--- run-python-wsl.cmd
+++ run-python-wsl.cmd
@@ -2,9 +2,9 @@
 setlocal
 call "%~dp0ancmv2_extra\init.cmd" WSL_PATH wsl.exe
 
-set IIS_SITE_PATH=%~dp0python
+set IIS_SITE_PATH=%~dp0python-wsl
 set LAUNCHER_PATH=%WSL_PATH%
-set LAUNCHER_ARGS=python/server.py
+set LAUNCHER_ARGS=ASPNETCORE_PORT=18000 python/server.py
 
 start http://localhost:50690/
 
